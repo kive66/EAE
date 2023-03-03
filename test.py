4263ea2394f99cde0a430b092054bce11fa39a74
kive66 <1246873045@qq.com>
2023-03-03 10:49:19 +0800

    add test file

---
+++ test.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+import re
+import numpy as np
+from transformers import AutoTokenizer
+import torch
+# line = "Cats are smarter than dogs"
+
+# matchObj = re.match(r'(.*) are (.*?) .*', line, re.M | re.I)
+
+# if matchObj:
+#    print( "matchObj.group() : ", matchObj.group())
+
+
+# l = torch.tensor([[1,0.6,0.4,1], [1,0,0,1]])
+# l[l>0.5]= 1
+# l = range(1,3)
+
+txt = 'dsf123as2sad'
+
+# print(re.findall(r'[0-9]+|[a-z]+', txt))
+# l = [[1,2],[3,4]]
+# print(torch.tensor(l))
+tokenizer = AutoTokenizer.from_pretrained('bert-large-uncased', do_lower_case=True) # 构建tokenizer
+t = ['','place of employment', 'placeofemployment']
+t = tokenizer(
+            t,
+            padding="max_length",
+            max_length=12,
+            truncation=True,
+            # return_token_type_ids=True,
+            # return_offsets_mapping = True,
+            is_split_into_words=True,
+            return_offsets_mapping=True,
+            return_tensors="pt"
+        )
+print(t)
+# t1 = tokenizer.tokenize(t[1])
+# print(t1)
+# 
+# event_path = "data/rams/event_role_multiplicities.txt"
+
+# def load_event_dict(path):
+#     event_dict = {}
+#     m = 0
+#     with open(path,'r',encoding='utf-8') as f:
+#         lines = f.readlines()
+#         for line in lines:
+#             roles = line.split()
+#             event_dict[roles[0]] = roles[1:]
+#             m = max(m, len(roles[1:]))
+#     print(m)
+#     return event_dict
+    
+# load_event_dict(event_path)
+
+# tokenizer = AutoTokenizer.from_pretrained("hfl/chinese-macbert-base", do_lower_case=True) # 构建tokenizer
+# t = ['你好吗',' 北京大饭店', '火车王']
+# t = tokenizer(
+#             t,
+#             padding="max_length",
+#             max_length=12,
+#             truncation=True,
+#             # return_token_type_ids=True,
+#             # return_offsets_mapping = True,
+#             return_tensors="pt"
+#         )
+# print(t)
+# srctxt = ['I like pizza', 'you hate dog']
+# print(tokenizer.tokenize(srctxt))
+
+# tokenized : 0:[ , ] 1:[ , ]
+# doc_token:  for i in range(offset):
---
+++ test2.py
@@ -0,0 +1,40 @@
+import torch
+
+# a= torch.randn((4,5,512))
+# b = a.reshape(4, 5*512)
+# print(a)
+
+# b[b>0] =1
+# print(b)
+# a = torch.ones((5,2,512,1))
+# b = torch.zeros((5,2,512,1))
+# c = torch.max(torch.cat((a,b), dim=-1),dim=-1)
+# print(c[0])
+
+# import platform
+# print('系统:',platform.system())
+
+# import time
+# T1 = time.perf_counter()
+
+# #______假设下面是程序部分______
+# for i in range(100*100):
+#     pass
+
+# T2 =time.perf_counter()
+# print('程序运行时间:%s毫秒' % ((T2 - T1)*1000))
+# t1 = torch.ones(6)
+# t1[1:3] = torch.zeros(2)
+# t2 = torch.ones(6)
+# t3 = torch.div(torch.add(t1,t2),0.5)
+# print(t3)
+# s = set([(1,2),(2,3),(1,2)])
+# s.update(s,[(1,2),(2,5)])
+# print((1,2)[1])
+# print(list(s))
+
+# t1 = torch.ones(6,device='cuda')
+# t2 = torch.ones(6,device='cuda')
+# l = [t1,t2]
+# print(torch.stack(l).device)
+d = {'true':['word','a'], 'pred':['word']}
